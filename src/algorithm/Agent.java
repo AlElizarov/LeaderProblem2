4195c90a59834e882ddd3381b38996d562b35923
Alexandr <Elizarov>
2016-02-28 16:11:20 +0300

    делал так что бы тест добавления срабатывал. Шаг TDD очень большой

---
+++ src/algorithm/Agent.java
@@ -0,0 +1,9 @@
+package algorithm;
+
+public class Agent {
+
+	public Agent(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/algorithm/MyRingList.java
+++ src/algorithm/MyRingList.java
@@ -2,8 +2,36 @@ package algorithm;
 
 public class MyRingList {
 
+	private int size;
+	private Node head;
+	private Node current;
+	private Node tail;
+
+	private class Node {
+		Agent info;
+		Node next;
+	}
+
 	public boolean isEmpty() {
-		return true;
+		return size == 0;
+	}
+
+	public void add(Agent agent) {
+		Node tmp = new Node();
+		tmp.info = agent;
+		if (size == 0) {
+			head = tmp;
+			current = head;
+			tail = tmp;
+		}
+		tail.next = tmp;
+		tail = tmp;
+		tmp.next = head;
+		size++;
+	}
+
+	public Object size() {
+		return size;
 	}
 
 }
