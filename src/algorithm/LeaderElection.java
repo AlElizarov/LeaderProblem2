1a7e19477c489ca73f67ef0428cae45364fb8bbb
Alexandr <Elizarov>
2016-03-01 13:55:31 +0300

    алгоритм теперь только решает задачу, весь вывод идет в GUI

--- src/algorithm/LeaderElection.java
+++ src/algorithm/LeaderElection.java
@@ -2,32 +2,16 @@ package algorithm;
 
 public class LeaderElection {
 
-	public static boolean solve(MyRingList list, int i) {
-		Agent next;
-		while (list.hasNext()) {
-			next = list.next();
-			if (i == 0) {
+	public static void solve(MyRingList list, int i) {
+		if (i == 0) {
+			Agent next;
+			while (list.hasNext()) {
+				next = list.next();
 				list.getNeiborough().setMsg(next.getId());
 				list.getNeiborough().setNewMsg(next.getId());
-				continue;
 			}
-//			if (list.getNeiborough().getId() == list.getNeiborough()
-//					.getNewMsg()) {
-//				while (list.hasNext()) {
-//					next = list.next();
-//					if (next.getId() < next.getMsg()) {
-//						list.getNeiborough().setNewMsg(next.getMsg());
-//					} else {
-//						list.getNeiborough().setNewMsg(0);
-//					}
-//				}
-//				list.setMessages();
-//				return true;
-//			}
-		}
-		if(i != 0)
-		list.setMessages();
-		return true;
+		} else
+			list.setMessages();
 	}
 
 }
