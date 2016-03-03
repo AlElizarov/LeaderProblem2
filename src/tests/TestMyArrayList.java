1e2ca3797f7f4cdba219003a7681bc7d78848f52
Alexandr <Elizarov>
2016-03-03 11:31:27 +0300

    Добавил тестовый класс для списка на основе массива

--- src/tests/TestMyArrayList.java
+++ src/tests/TestMyArrayList.java
@@ -4,7 +4,11 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import algorithm.MyAbstractList;
+
 public class TestMyArrayList {
+	
+	private MyAbstractList list;
 
 	@Test
 	public void test() {
