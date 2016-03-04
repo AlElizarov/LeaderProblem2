6ce7f4b390f8c516975154ef0fe9b50a61b9c51a
Alexandr <Elizarov>
2016-03-04 14:09:02 +0300

    мелкие тестовые изменения

--- src/algorithm/BiDirectLeaderElection.java
+++ src/algorithm/BiDirectLeaderElection.java
@@ -37,7 +37,6 @@ public class BiDirectLeaderElection {
 
 	private static void sendLeft(MyAbstractList list, int stage, int step) {
 		list.setCorrectLeftSendersIndexes();
-		System.out.println("LSinSend"+list.getLeftSenders());
 		for (int i = 0; i < list.getLeftSenders().size(); i++) {
 			list.get(list.getLeftSenders().get(i) + 1).setNewLeftMsg(
 					list.get(list.getLeftSenders().get(i)).getLeftMsg());
@@ -52,14 +51,11 @@ public class BiDirectLeaderElection {
 
 	private static void sendRight(MyAbstractList list, int stage, int step) {
 		list.setCorrectRightSendersIndexes();
-		System.out.println("RSinSend"+list.getRightSenders());
-		
 		for (int i = 0; i < list.getRightSenders().size(); i++) {
 			list.get(list.getRightSenders().get(i) - 1).setNewRightMsg(
 					list.get(list.getRightSenders().get(i)).getRightMsg());
 		}
 		list.setRighMsgs();
-		System.out.println("messagesInSend"+list.printLeftAndRightMsgs());
 		if ((int) Math.pow(2, stage) - step == 1) {
 			list.setLeftRequesters();
 		} else {
@@ -74,18 +70,18 @@ public class BiDirectLeaderElection {
 	
 	public static void main(String[] args){
 		MyAbstractList list = new MyRingArrayList();
-		list.add(new Agent(12));
-		list.add(new Agent(11));
-		list.add(new Agent(10));
-		list.add(new Agent(9));
-		list.add(new Agent(8));
-		list.add(new Agent(7));
-		list.add(new Agent(6));
-		list.add(new Agent(5));
 		list.add(new Agent(4));
+		list.add(new Agent(7));
 		list.add(new Agent(3));
-		list.add(new Agent(2));
+		list.add(new Agent(5));
 		list.add(new Agent(1));
+		list.add(new Agent(8));
+		list.add(new Agent(2));
+		list.add(new Agent(6));
+	list.add(new Agent(14));
+//		list.add(new Agent(3));
+//		list.add(new Agent(2));
+//		list.add(new Agent(110));
 		//System.out.println((int)(Math.log(list.size())/Math.log(2)));
 		int loopEnd = (int)(Math.log(list.size())/Math.log(2));
 		if((list.size() & (list.size()-1)) != 0){
@@ -94,7 +90,11 @@ public class BiDirectLeaderElection {
 		for(int i = 0; i < loopEnd; i++){
 			for(int j = 0; j <= Math.pow(2, i); j++ ){
 				choice(list, i ,j);
+				System.out.println("LS"+list.getLeftSenders());
+				
+				//System.out.println(list.getRightSenders());
 			}
+			//System.out.println("CL"+list.getCurrentLeaders());
 		}
 		
 //		choice(list, 0, 0);
