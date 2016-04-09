b744dd7fee47d04420c088df83282d69b806fcf5
Alexandr <Elizarov>
2016-04-09 09:39:25 +0300

    Еще больше перенес логику в листы

--- src/algorithm/LeaderElection.java
+++ src/algorithm/LeaderElection.java
@@ -4,14 +4,10 @@ public class LeaderElection {
 
 	public static void solve(MyAbstractList list, int i) {
 		if (i == 0) {
-			Agent next;
-			while (list.hasNext()) {
-				next = list.next();
-				list.getNextNeiborough().setLeftMsg(next.getId());
-				list.getNextNeiborough().setNewLeftMsg(next.getId());
-			}
-		} else
+			list.initiateStartState();
+		} else {
 			list.setMessages();
+		}
 	}
 
 }
