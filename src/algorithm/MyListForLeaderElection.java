47092e4364add54bb45d61757ab3976477536c4f
Alexandr <Elizarov>
2016-04-10 23:27:06 +0300

    Оставил только один интерфейс для лсита

--- src/algorithm/MyListForLeaderElection.java
+++ src/algorithm/MyListForLeaderElection.java
@@ -1,9 +1,29 @@
 package algorithm;
 
-public interface MyListForLeaderElection<T> extends MyList<T>{
-	
+public interface MyListForLeaderElection<T> {
+
+	boolean isEmpty();
+
+	int size();
+
+	void add(T agent);
+
+	void add(int item);
+
+	void add(String item);
+
+	void addAll(T[] data);
+
+	void addAll(int[] data);
+
+	void addAll(String[] data);
+
+	T next();
+
+	boolean hasNext();
+
+	T get(int idx);
+
 	T getNextNeiborough();
-	public boolean hasSolution();
-	public int getLeaderId();
 
 }
