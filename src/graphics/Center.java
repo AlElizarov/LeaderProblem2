971bf5f4e7f5f20a013d99d87c6eac8d5313a940
Alexandr <Elizarov>
2016-04-15 15:30:26 +0300

    Еще один Value object. Но все мои Value object-ы имеют так же и полезные функции

--- src/graphics/Center.java
+++ src/graphics/Center.java
@@ -1,12 +1,14 @@
 package graphics;
 
-public class MyCoord {
+import utils.Trigonometry;
+
+public class Center {
 
 	private int roundCenterX;
 	private int roundCenterY;
 	private int radius;
 
-	public MyCoord(int roundCenterX, int roundCenterY, int radius) {
+	public Center(int roundCenterX, int roundCenterY, int radius) {
 		this.roundCenterX = roundCenterX;
 		this.roundCenterY = roundCenterY;
 		this.radius = radius;
@@ -16,24 +18,22 @@ public class MyCoord {
 		return roundCenterX;
 	}
 
-	public void setRoundCenterX(int roundCenterX) {
-		this.roundCenterX = roundCenterX;
-	}
-
 	public int getRoundCenterY() {
 		return roundCenterY;
 	}
 
-	public void setRoundCenterY(int roundCenterY) {
-		this.roundCenterY = roundCenterY;
+	public int calculateXPos(int quantityOfNodes, int currentIdx) {
+		double angle = currentIdx * (360.0 / quantityOfNodes);
+		double shift = radius * Trigonometry.cos(angle);
+		int newXPos = (int) (roundCenterX + shift);
+		return newXPos;
 	}
 
-	public int getRadius() {
-		return radius;
-	}
-
-	public void setRadius(int radius) {
-		this.radius = radius;
+	public int calculateYPos(int quantityOfNodes, double currentIdx) {
+		double angle = currentIdx * (360.0 / quantityOfNodes);
+		double shift = radius * Trigonometry.sin(angle);
+		int newYPos = (int) (roundCenterY + shift);
+		return newYPos;
 	}
 
 }
