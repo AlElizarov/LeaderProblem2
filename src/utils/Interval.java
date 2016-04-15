d7a17bfedae36892ec66fdf8fb0598189263875f
Alexandr <Elizarov>
2016-04-15 15:23:46 +0300

    Добавил Value Object для упрощения клиентского кода

---
+++ src/utils/Interval.java
@@ -0,0 +1,34 @@
+package utils;
+
+public class Interval {
+
+	private int start;
+	private int end;
+
+	public Interval(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+
+	public int getStart() {
+		return start;
+	}
+
+	public int getEnd() {
+		return end;
+	}
+
+	public void moveTop(int shift) {
+		start += shift;
+		end += shift;
+	}
+
+	public int getCenter() {
+		return (start+end)/2;
+	}
+
+	public Interval turnBack() {
+		return new Interval(end, start);
+	}
+
+}
---
+++ src/utils/LineSegment.java
@@ -0,0 +1,41 @@
+package utils;
+
+public class LineSegment {
+
+	private Interval xInterval;
+	private Interval yInterval;
+
+	public LineSegment(Interval xInterval, Interval yInterval) {
+		this.xInterval = xInterval;
+		this.yInterval = yInterval;
+	}
+
+	public Interval getXInterval() {
+		return xInterval;
+	}
+
+	public Interval getYInterval() {
+		return yInterval;
+	}
+
+	public void moveXRight(int shift) {
+		xInterval.moveTop(shift);
+	}
+
+	public void moveYTop(int shift) {
+		yInterval.moveTop(shift);
+	}
+
+	public int getCenterX() {
+		return xInterval.getCenter();
+	}
+
+	public int getCenterY() {
+		return yInterval.getCenter();
+	}
+
+	public LineSegment turnBack() {
+		return new LineSegment(xInterval.turnBack(), yInterval.turnBack());
+	}
+
+}
